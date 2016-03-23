50aa4925d78b16b08d2b0c6d4a5e678e94373b2c
Vinicius Silva <viniciuspontocom@gmail.com>
2016-03-22 23:51:11 -0300

    Add validations for the make and model models

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,2 +1,6 @@
 class Make < ActiveRecord::Base
+    has_many :models
+
+    validates_presence_of :name, message: "is missing"
+    validates_presence_of :webmotors_id, message: "is missing"
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,6 @@
 class Model < ActiveRecord::Base
+    belongs_to :make
+
+    validates_presence_of :make_id, message: "is missing"
+    validates_presence_of :name, message: "is missing"
 end
