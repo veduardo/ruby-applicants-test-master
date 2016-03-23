87b87c24c5152ac117ef820b44407e296930d7af
Vinicius Silva <viniciuspontocom@gmail.com>
2016-03-23 01:36:36 -0300

    Add a check for an empty maker

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -3,19 +3,23 @@ class ModelsController < ApplicationController
     #search the models
     uri = URI("http://www.webmotors.com.br/carro/modelos")
 
-    make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
+    if params[:webmotors_make_id] != "0"
+      make = Make.where(webmotors_id: params[:webmotors_make_id])[0]
 
-    # Make request for Webmotors site
-    response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
-    models_json = JSON.parse response.body
+      # Make request for Webmotors site
+      response = Net::HTTP.post_form(uri, { marca: params[:webmotors_make_id] })
+      models_json = JSON.parse response.body
 
-    # debugger
+      # debugger
 
-    # Itera no resultado e grava os modelos que ainda não estão persistidas
-    models_json.each do |json|
-      if Model.where(name: json["Nome"], make_id: make.id).size == 0
-        Model.create(make_id: make.id, name: json["Nome"])
+      # Itera no resultado e grava os modelos que ainda não estão persistidas
+      models_json.each do |json|
+        if Model.where(name: json["Nome"], make_id: make.id).size == 0
+          Model.create(make_id: make.id, name: json["Nome"])
+        end
       end
+    else
+      redirect_to :back
     end
   end
 end
